c71b326ed7f9d3ef9747b99d71b2a404d092d300
oli4wolf <34458202+oli4wolf@users.noreply.github.com>
2024-12-05 10:04:22 +0100

    Changed from print to logging.

--- downloader.py
+++ downloader.py
@@ -13,23 +13,26 @@ def main(auth_token: str, days: int):
     
     # Loop through last X days
     for i in range(days):
+        # Create the call
         datePlus = current_date - timedelta(days=i)
         date = current_date - timedelta(days=i+1)
         head = {'Authorization': 'Bearer {}'.format(auth_token)} #Authorization: Bearer
         url = f'{base_url}?output_mode=csv&search=search index=sbb_journey-service_internal_prod_events "Analytics" &earliest_time={date.year}-{date.month}-{date.day}T00%3A00%3A00.000-02%3A00&latest_time={datePlus.year}-{datePlus.month}-{datePlus.day}T00%3A00%3A00.000-02%3A00&loglevel=INFO'
-        print(url)
+        logging.info(url)
         output_filename = f'./download/output{date.year}{date.month}{date.day}'
         response = requests.get(url, headers=head)
 
+        # Evaluate the Response
         if response.status_code == 200:
             with open(f'{output_filename}.csv', 'w', encoding='utf-8') as f:
                 for line in response.text.splitlines():
                     line = extract_data(line)
                     if line != None:
                         f.write(line + '\n')
+        #Handle error but log it.
         else:
-            print(f"Error: Status code {response.status_code}")
-            print(response.text)
+            logging.error(f"Error: Status code {response.status_code}")
+            logging.error(response.text)
             with open(f'{output_filename}-Error-{response.status_code}.txt', 'w', encoding='utf-8') as f:
                 f.write(response.text)
 
